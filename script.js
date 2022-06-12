c4c2c9caa57ba162f4750ca7bb63b342e0a4f1d1
ariya <ariya_watchara@cmu.ac.th>
2022-06-12 21:49:12 +0700

    Movie detail

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 var searchResultDisplay = document.getElementById('search_result')
+var singleAnimeDisplay = document.getElementById('single_anime_result')
 
 function hideAll(){
     searchResultDisplay.style.display='none'
+    singleAnimeDisplay.style.display='none'
 }
+
 document.getElementById('homeButton').addEventListener('click', function(){
     hideAll()
 })
-
+function singleAnimeData(anime){
+    hideAll()
+    singleAnimeDisplay.style.display='block'
+    let imageElem = document.getElementById('image')
+    imageElem.setAttribute('src', anime.images.jpg.image_url)
+    let titleElem = document.getElementById('title')
+    titleElem.innerHTML = anime.title
+    let titleJapanElem = document.getElementById('title_japanese')
+    titleJapanElem.innerHTML = anime.title_japanese
+    let episodesElem = document.getElementById('episodes')
+    episodesElem.innerHTML = anime.episodes
+    let scoreElem = document.getElementById('score')
+    scoreElem.innerHTML = anime.score
+    let statusElem = document.getElementById('status')
+    statusElem.innerHTML = anime.status
+    let synopsisElem = document.getElementById('synopsis')
+    synopsisElem.innerHTML = anime.synopsis
+}
 document.getElementById('searchButton').addEventListener('click',() =>{
     const query = document.getElementById('inputText').value
     console.log(query)
@@ -38,6 +58,8 @@ function displaySearchResult(anime){
     colDiv.classList.add('col') 
     let firstDiv = document.createElement('div')
     firstDiv.classList.add('card')
+    firstDiv.addEventListener('dblclick', function() {
+    })
     colDiv.appendChild(firstDiv)
     let image = document.createElement('img')
     image.setAttribute('src',anime.images.jpg.image_url)
@@ -56,6 +78,15 @@ function displaySearchResult(anime){
     paragraph.classList.add('text-secondary')
     paragraph.innerHTML = anime.status
     secondDiv.appendChild(paragraph)
+    let button = document.createElement('button')
+    button.classList.add('btn')
+    button.classList.add('btn-warning')
+    button.setAttribute('type', 'button')
+    button.innerText = 'detail'
+    button.addEventListener('click', function(){
+        singleAnimeData(anime)
+    })
+    secondDiv.appendChild(button)
     searchResult.appendChild(colDiv)
 }
 function onLoad(){
